5b223794c29ea529e66040f99c1b4da7c32a7a02
rakibul <rakibulislamrabby64@gmail.com>
2025-05-18 13:37:55 +0600

    make small components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import Header from './components/shared/Header'
 import Footer from './components/shared/Footer'
 import HeroSection from './components/HeroSection'
+import TaskBoard from './components/task/TaskBoard'
 function App() {
 
   return (
-   <div>
+   <div className='px-28'>
     <Header />
+    <div className='flex flex-col justify-center items-center'>
     <HeroSection />
+    <TaskBoard />
+    </div>
     <Footer />
    </div>
   )
