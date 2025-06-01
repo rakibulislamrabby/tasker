dd237670fab1d1018112dbcd6f4afca91c79f01e
rakibul <rakibulislamrabby64@gmail.com>
2025-06-01 09:58:13 +0600

    add icons rendaring

--- src/components/task/TaskAction.jsx
+++ src/components/task/TaskAction.jsx
@@ -5,12 +5,13 @@ export default function TaskAction() {
   const addTask = () => {
     console.log('add task')
     setIsModalOpen(true)
+    console.log(isModalOpen)
   }
   return (
     <div className="mb-14 items-center justify-between sm:flex">
               <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
               <div className="flex items-center space-x-5">
-                <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+                <button onClick={addTask} className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
                   Add Task
                 </button>
                 <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
--- src/components/task/TaskList.jsx
+++ src/components/task/TaskList.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 import tasksData from '../../data/tasks.json'
+import { IoMdStarOutline } from 'react-icons/io'
+import { IoStar } from 'react-icons/io5'
 
 export default function TaskList() {
-  const StarIcon = ({ isFavorite }) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-star"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke={isFavorite ? "yellow" : "currentColor"}
-      fill={isFavorite ? "yellow" : "none"}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-    </svg>
-  )
 
   return (
-    <div className="overflow-auto">
+    <div>
       <table className="table-fixed overflow-auto xl:w-full">
         <thead>
           <tr>
@@ -52,7 +37,7 @@ export default function TaskList() {
           {tasksData.map((task) => (
             <tr key={task.id} className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
               <td>
-                <StarIcon isFavorite={task.isFavorite} />
+                {task.isFavorite ?<IoStar className='text-yellow-500' /> :<IoMdStarOutline /> }
               </td>
               <td>{task.title}</td>
               <td>
